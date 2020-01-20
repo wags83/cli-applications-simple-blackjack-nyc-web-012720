39789b195d0b5325a588b7784aecccdf135e8756
Greg Wagner <gpwagner@gmail.com>
2020-01-20 08:50:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,8 @@ def end_game (card_total)
 end
 
 def initial_round
-  card_total = deal_card + deal_card
+  card_total = deal_card
+  card_total += deal_card
   display_card_total (card_total)
  end
 
