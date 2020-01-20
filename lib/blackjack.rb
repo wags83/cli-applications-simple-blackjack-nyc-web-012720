202def6106d21471a82f4a920cf4d57d99db22aa
Greg Wagner <gpwagner@gmail.com>
2020-01-20 08:59:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def hit? (card_total)
  prompt_user
  user_input = get_user_input
  if user_input == "h"
-   card_total += deal card
+   card_total += deal_card
  elsif user_input == "s"
  elsif
    invalid_command
